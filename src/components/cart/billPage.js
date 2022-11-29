97ca6083fa9954c6865b41bd65f4aa0fb161412c
Arman Ebrahimi <ebrahimi.arman72@gmail.com>
2022-11-28 18:15:50 -0800

    added some responsiveness and new features

--- src/components/cart/billPage.js
+++ src/components/cart/billPage.js
@@ -10,9 +10,9 @@ export const BillPage = ({totalPrice, closeBill}) => {
             <p><span>Tax: </span><span>3%</span></p>
             <p><span>Transport cost: </span><span>$2</span></p>
             <p className="text-center">Final price: ${(totalPrice + totalPrice * 0.03 + 2).toFixed(2)}</p>
-            <form className="d-flex justify-content-between align-items-center">
+            <form>
                 <label>Please enter your address:</label>
-                <textarea className="w-75"></textarea>
+                <textarea className="w-75 mt-1"></textarea>
             </form><br />
             <button className="btn btn-success">Payment</button>
         </>
--- src/components/product/quickView.js
+++ src/components/product/quickView.js
@@ -1,6 +1,14 @@
-
+import {useState} from "react";
 
 export const QuickView = ({isShow, product, handleClose}) => {
+    const [bigImage, setBigImage] = useState({show: false, index: 0});
+
+    const handleShowImage = (index) => {
+        setBigImage({show: true, index: index});
+    }
+    const closeImage = () => {
+        setBigImage({...bigImage, show: false});
+    }
     return(
         <div className={`detail-page ${isShow && "show-detail"}`}>
             <button className="close-detail" onClick={handleClose}>&#x2715;</button>
@@ -11,9 +19,14 @@ export const QuickView = ({isShow, product, handleClose}) => {
                 })}
             </ul>
             <div className="d-flex justify-content-around">
-                <img src={product.thumbnails[0]} alt="image1" />
-                <img src={product.thumbnails[1]} alt="image2" />
-                <img src={product.thumbnails[2]} alt="image3" />
+                {product.thumbnails.map((item, index) => {
+                    return(
+                        <img className="thumb-img" src={item} alt="" onMouseOver={() => handleShowImage(index)} onMouseOut={closeImage} />
+                    )
+                })}
+            </div>
+            <div className={`focused-img ${bigImage.show && "show-focused-img"}`}>
+                <img src={product.thumbnails[bigImage.index]} width="100%" height="100%" alt="" />
             </div>
         </div>
     )
